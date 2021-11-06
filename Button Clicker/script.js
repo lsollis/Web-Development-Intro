2bb7875878d7ecbc95d8d87ceb2000e13787c341
lsollis <92566497+lsollis@users.noreply.github.com>
2021-11-05 22:22:36 -1000

    Button Clicker assignment completed

---
+++ Button Clicker/script.js
@@ -0,0 +1,14 @@
+function hide(element) {
+    element.remove();
+}
+
+function logout() {
+    var change = document.getElementById("login");
+    if (change.innerHTML== "Login") {
+        change.innerHTML = "Logout";
+    }
+    else {
+        change.innerHTML = "Login";
+    }
+    console.log("signout")
+}
